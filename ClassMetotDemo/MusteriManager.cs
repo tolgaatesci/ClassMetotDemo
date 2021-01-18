5ca117ec2dd2f0d00be9e26d97cbaa5738c2e946
Tolga Ateşci <x-man_3737@hotmail.com>
2021-01-19 00:25:45 +0300

    Son hali.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteri eklendi : " + " " + musteri.Id + " " + " " + musteri.Adi);            
+        }
+
+        public void Listele(Musteri[] musteriler) 
+        {
+            
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri listesi : " + musteri.Id + " " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Cinsiyeti + " " + musteri.Meslegi + " " + musteri.KrediNotu);
+            }
+        }
+
+        public void Silme(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Id + " numaralı müsteri silindi. Silinen musterinin adi : " + musteri.Adi);            
+        }
+
+        public void KrediVerilebilirMi(Musteri musteri)
+        {
+            int krediNotu = musteri.KrediNotu;
+            
+            if( krediNotu < 700 )
+            {
+                Console.WriteLine("Müşterimiz olan " + " " + musteri.Adi + " " + musteri.Soyadi + " kredi almaya elverişli değildir.");
+            }
+            else
+            {
+                Console.WriteLine("Müşterimiz olan " + " " + musteri.Adi + " " + musteri.Soyadi + " kredi almaya elverişlidir.");
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,51 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 123;
+            musteri1.Adi = "Tolga";
+            musteri1.Soyadi = "Ateşci";
+            musteri1.Cinsiyeti = "E";
+            musteri1.Meslegi = "Elektronik Mühendisi";
+            musteri1.KrediNotu = 500;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 456;
+            musteri2.Adi = "Ahmet";
+            musteri2.Soyadi = "Ateşci";
+            musteri2.Cinsiyeti = "E";
+            musteri2.Meslegi = "Kamyon Şoförü";
+            musteri2.KrediNotu = 1600;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 789;
+            musteri3.Adi = "Banu";
+            musteri3.Soyadi = "Ateşci";
+            musteri3.Cinsiyeti = "K";
+            musteri3.Meslegi = "Ev Hanımı";
+            musteri3.KrediNotu = 710;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            
+            musteriManager.Listele(musteriler);
+            Console.WriteLine("\n");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("\n");
+            musteriManager.Ekle(musteri2);
+            Console.WriteLine("\n");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("\n");
+            musteriManager.KrediVerilebilirMi(musteri2);
+            Console.WriteLine("\n");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("\n");
+            musteriManager.Silme(musteri1);
+            Console.WriteLine("\n");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("\n");
+
         }
     }
 }
